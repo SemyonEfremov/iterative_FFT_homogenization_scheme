0507bdf8803381adbecbb6e88ac2faa23509a0fd
SemyonEfremov <82779976+SemyonEfremov@users.noreply.github.com>
2022-12-20 19:21:21 +0100

    commit by SE

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import numpy as np
+import cupy as cp
 
+memory = cp.get_default_memory_pool()
+pinned_memory = cp.get_default_pinned_memory_pool()
+print(cp.get_default_memory_pool().get_limit())
+print(pinned_memory.n_free_blocks())
+a = cp.zeros((100,100))
+print(a.nbytes)
+print(memory.total_bytes())
 
 if __name__ == "__main__":
-    a = 5
-    print(a)
+    pass
