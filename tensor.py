3d4c1b08c553d778318ceb451c785b146fe5989b
SemyonEfremov <82779976+SemyonEfremov@users.noreply.github.com>
2023-01-11 22:53:38 +0100

    push by SE

--- tensor.py
+++ tensor.py
@@ -4,35 +4,40 @@ import cupy as cp
 class Tensor(object):
 
     def __init__(self, order_values: np.ndarray, spatial_dimensions: np.ndarray) -> None:
-        self._order = order_values
-        self._dimensions = spatial_dimensions
-        self._tensor_values = np.zeros((np.concatenate((self._order, self._dimensions), axis = 0)))
+        self.order = order_values
+        self.dimensions = spatial_dimensions
+        self.tensor_values = np.zeros((np.concatenate((self.order, self.dimensions), axis = 0)))
 
     def set_mean_value(self, mean_value: np.ndarray) -> None:
-        pass
+        current_mean_value = self.calculate_mean_value()
+        mean_value_delta = current_mean_value - mean_value
+        tensor_values_gpu = cp.array(self.tensor_values) - cp.array(mean_value_delta)
+        self.tensor_values = cp.asnumpy(tensor_values_gpu)
+        del tensor_values_gpu
 
     def calculate_mean_value(self) -> np.ndarray:
-        return np.zeros(1)
-
-    @staticmethod
-    def calculate_in_product(t_values_a: Tensor, t_values_b: Tensor)
-        pass
+        along_axes = tuple(range(-1, -(len(self.dimensions) + 1), -1))
+        mean_value_gpu = cp.mean(cp.array(self.tensor_values), along_axes)
+        mean_value_cpu = cp.asnumpy(mean_value_gpu)
+        del mean_value_gpu
+        return mean_value_cpu
 
     def compute_ffourier(self) -> np.ndarray:
+        tensor_values_gpu = cp.array(self.tensor_values)
         return np.zeros(1)
 
     def compute_bfourier(self) -> np.ndarray:
         return np.zeros(1)
 
     def get_info(self) -> tuple:
-        return self._tensor_values, self._order, self._dimensions
+        return self.tensor_values, self.order, self.dimensions
 
 if __name__ == "__main__":
     t_order = np.array([6,1])
     t_dimensions = np.array([2, 2, 2])
     print(t_order, t_dimensions)
     t_tensor = Tensor(t_order, t_dimensions)
-    temp = t_tensor._tensor_values
+    temp = t_tensor.tensor_values
     print(temp[:,:,0,0,0])
     temp[0,0,0,0,0] = 1
-    print(t_tensor._tensor_values[:,:,0,0,0])
+    print(t_tensor.tensor_values[:,:,0,0,0])
